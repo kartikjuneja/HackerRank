f6b2008b203cb0b46d691e9c111869bdde727a83
Kartik Juneja <kjuneja7004@gmail.com>
2022-12-01 19:51:15 +0530

    Create Jumping on the Clouds.cs

---
+++ Interview Preparation Kit/Warm-up Challenges/Jumping on the Clouds.cs
@@ -0,0 +1,62 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'jumpingOnClouds' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY c as parameter.
+     */
+
+    public static int jumpingOnClouds(List<int> c)
+    {
+        int jumpCount=0;
+        for(int i = 0; i < c.Count;i++){
+            if(i + 2 >= c.Count)
+            {
+                if(i+1>=c.Count){
+                    break;
+                }
+            }
+            else if(c[i+2] == 0){
+                  i++;
+            }
+            jumpCount+=1;
+        }
+        return jumpCount;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> c = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(cTemp => Convert.ToInt32(cTemp)).ToList();
+
+        int result = Result.jumpingOnClouds(c);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
