6f239c602ac6439d14ed029fa2f565a994ae188b
Kartik Juneja <kjuneja7004@gmail.com>
2022-12-05 12:33:11 +0530

    Create Mini-Max Sum.cs

---
+++ Interview Preparation Kits/1 Week Preparation Kit/Day 1/Mini-Max Sum.cs
@@ -0,0 +1,56 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
+    {
+        long minimum = long.MaxValue;
+        long maximum = long.MinValue;
+        long sum = 0;
+        foreach(int value in arr){
+            
+            if(value<minimum){
+                minimum = value;
+            }
+            
+            if(value>maximum){
+                maximum = value;
+            }
+            
+            sum +=value;
+        }
+        
+        Console.Write((sum-maximum) +" "+ (sum-minimum));
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
+}
